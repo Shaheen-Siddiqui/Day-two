ce870e71422eccf83be239ef2db1af6adc767437
Shaheen-Siddiqui <95218912+Shaheen-Siddiqui@users.noreply.github.com>
2022-01-12 14:49:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Compo from './Pera';
+
+const App=()=>{
+  return (
+          <>
+          <Compo />
+          
+          </>
+          )
+}
+export default App;
